3272d48c8c17d863bd9c75ad163ad0c15e44f20a
Abdullah-1360 <abdullah.shah78963@gmail.com>
2026-07-26 16:53:43 +0500

    feat(router): update __init__.py module

--- router/__init__.py
+++ router/__init__.py
@@ -3,7 +3,11 @@ router/__init__.py
 Exports the main RouterNode and convenience helpers.
 """
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from .router import RouterNode
 from .db import get_pool, close_pool
+from .chat_model import ChatRouter
 
-__all__ = ["RouterNode", "get_pool", "close_pool"]
+__all__ = ["RouterNode", "get_pool", "close_pool", "ChatRouter"]
